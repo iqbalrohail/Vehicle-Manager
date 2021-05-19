41ea8fe1fde5f22b192e098b460fadf81e049acf
Rohail <iqbalrohail398@gmail.com>
2021-05-19 21:08:55 +0000

    version view test

--- src/main/java/com/vehicle/manager/controller/view.java
+++ src/main/java/com/vehicle/manager/controller/view.java
@@ -3,14 +3,15 @@ package com.vehicle.manager.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.io.Console;
+
 @Controller
 public class view {
 
     @GetMapping("/index")
     public String view()
     {
-
-
+        System.out.println("hello git ");
         return "index";
     }
 
